9b483639af909b68ab771dcce1b493a58ea0a66c
Rajiv-test <tathireddyrajivreddy@gmail.com>
2025-03-14 20:20:58 +0530

    added cacheing

--- command_map.go
+++ command_map.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+
 )
 
 func commandMapf(cfg *config) error {
@@ -24,7 +25,6 @@ func commandMapb(cfg *config) error {
 	if cfg.prevLocationsURL == nil {
 		return errors.New("you're on the first page")
 	}
-
 	locationResp, err := cfg.pokeapiClient.ListLocations(cfg.prevLocationsURL)
 	if err != nil {
 		return err
